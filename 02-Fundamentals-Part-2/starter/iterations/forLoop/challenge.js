8dfc39b07a06294965496d3fea26e74db239c64e
manoj <manoj.reddy@albanero.io>
2026-05-21 09:02:58 +0530

    Iterations, Section2 completed

---
+++ 02-Fundamentals-Part-2/starter/iterations/forLoop/challenge.js
@@ -0,0 +1,26 @@
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+/* Write your code below. Good luck! 🙂 */
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips[i] = calcTip(bills[i]);
+  totals[i] = bills[i] + tips[i];
+}
+// console.log(bills, tips, totals);
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
